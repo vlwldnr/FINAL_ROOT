bd08e89e85cc8e243549dac2382e09532bdfd529
vlwldnr <vlwldnr@Naver.com>
2015-06-20 03:00:18 +0900

    Update get_enable.sh

--- get_enable.sh
+++ get_enable.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-while sleep 10; do /var/lib/tomcat7/webapps/ROOT/libcoap/coap-client -m get -o /var/lib/tomcat7/webapps/ROOT/libcoap/output.txt coap://[bbbb::1415:9200:0017:9e13]/pir; done &
+while sleep 10; do /var/lib/tomcat7/webapps/ROOT/libcoap/coap-client 
+-m get -o /var/lib/tomcat7/webapps/ROOT/libcoap/output.txt coap://[bbbb::1415:9200:0017:9e13]/pir; done &
